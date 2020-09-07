04fe5cf69c92ba60971c34ac88ff1c910c495ab4
Jaap Koetsier <jaap@chariot-it.nl>
2020-09-07 10:18:57 +0200

    Increase version number

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class PyTest(TestCommand):
 with codecs.open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
-version = '2019.04.08'
+version = '2020.09.07'
 
 packages = ['objectstore']
 
